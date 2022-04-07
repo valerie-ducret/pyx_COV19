e9d0f38084d33c47e41d8a7381e72f56fd2d51b2
Dr Valérie Ducret <valerie.ducret@gmail.com>
2022-04-07 11:32:05 +0200

    remove h5py package

--- modules/img_classification.py
+++ modules/img_classification.py
@@ -8,11 +8,12 @@ Original file is located at
 """
 
 import keras
+from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
 import streamlit as st
 from pathlib import Path
-import h5py
+#import h5py
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 
